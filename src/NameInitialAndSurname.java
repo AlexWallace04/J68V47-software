53f1d51dd83e16e61d03963fac588fc127f77757
30142320 <30142320@nescol.ac.uk>
2023-09-20 13:43:44 +0100

    password

--- src/NameInitialAndSurname.java
+++ src/NameInitialAndSurname.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class NameInitialAndSurname {
     public static void main(String[] args) {
-        // Create a Scanner object to read user input
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user for their first name
+        // Input: First Name
         System.out.print("Enter your first name: ");
         String firstName = scanner.nextLine();
 
-        // Prompt the user for their surname
-        System.out.print("Enter your surname: ");
-        String surname = scanner.nextLine();
+        // Input: Last Name
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
 
-        // Prompt the user for their year of birth
+        // Input: Year of Birth
         System.out.print("Enter your year of birth: ");
         int yearOfBirth = scanner.nextInt();
 
+        // Generate Username and Password
+        String username = firstName.substring(0, 1).toLowerCase() + lastName.toLowerCase();
+        String password = lastName.substring(0, 1).toLowerCase() + firstName.toUpperCase() + yearOfBirth;
+
+        // Display Username and Password
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
         // Close the scanner
         scanner.close();
-
-        // Get the first letter of the first name in uppercase
-        char firstInitial = Character.toUpperCase(firstName.charAt(0));
-
-        // Display the first letter of the first name in uppercase and the whole surname
-        System.out.println("First letter of your name in uppercase: " + firstInitial);
-        System.out.println("Your surname: " + surname);
     }
 }
